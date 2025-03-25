21b8fe1ef8823eb985250bbc09f8dc2b01b72542
Aneetam <88475279+Aneetam@users.noreply.github.com>
2025-03-25 10:27:49 +0530

    Add files via upload

---
+++ Accessing Instance Attributes.py
@@ -0,0 +1,11 @@
+class Cart:
+  flat_discount = 0
+  min_bill = 100
+  def __init__(self):
+      self.items = {}
+  def add_item(self,..):
+      self.items[item_name] = quantity
+  def display_items(self):
+      print(items)
+a = Cart()
+a.display_items()
